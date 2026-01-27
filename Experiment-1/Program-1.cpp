e484d494e38a6f4043ae3e965f57ad34bed9c157
Gnan Parekh <gnan0839@gmail.com>
2026-01-26 23:10:13 -0800

    Add Program-1.cpp for input and output demonstration

---
+++ Experiment-1/Program-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+
+    cin >> a >> b >> c;
+    cout << "Integer :" << a << endl;
+    cout << "Character :" << b << endl;
+    cout << "Float :" << c << endl;
+
+    return 0;
+}
